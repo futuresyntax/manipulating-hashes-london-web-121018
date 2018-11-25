b3936219cce71fbfa61ce50fcbd0c1e18ff8a289
FutureScript <fernandobaxter1@gmail.com>
2018-11-25 23:24:39 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,4 +1,6 @@
-def second_challenge
+
+
+ def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
    vegetable: ["carrots", "broccoli", "cucumbers"],
@@ -6,6 +8,9 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
+  array = groceries.values
+    	array.flatten
+   end
   #code your solution here!
 
   
